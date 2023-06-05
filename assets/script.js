bbc7f70f0fa5f90066ecdfe18176281a5416a726
Delante Randolph <delantetr@Delantes-iMac.local>
2023-06-05 17:53:17 -0400

    Code update/adjustments

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 
@@ -7,61 +8,48 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   
   var userInput = prompt("How many characters will your password be?", "Must be between 8 and 128 characters");
+  console.log("User Input: " + userInput)
 
   if(userInput < 8 || userInput > 128) {
     alert("Please select a number between 8 and 128.");
   } else {
       var lowerCase = confirm("Would you like to include lower case characters?")
+      console.log("Lower Case: " + lowerCase);
       var upperCase = confirm("Would you like to include upperCase characters?")
+      console.log("Upper Case: " + upperCase);
       var numeric = confirm("Would you like to include numeric characters?")
+      console.log("Numeric: " + numeric);
       var specialConfirm = confirm("Would you like to use special characters? (i.e. $,%,&)")
+      console.log("Special Charac: " + specialConfirm);
   }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
-
-function generatePassword() {
-  
-  var password = []
-
-  if(userInput > 8 || userInput < 128) {
-    password.push(...userInput)
-  }
-  if(lowerCase) {
-    var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    password.push(...lowerLetters)
-  }
-  if(upperCase) {
-    var upperLetters = lowerLetters.toUpperCase()
-    password.push(...upperLetters)
-  }
-  
-  password.textContent = password
-}
-
-
-
-
-
-
-
-
-
-
-
-  
-
-//   function password(){
-    
-//     if(userInput > 8 || userInput < 128 && lowerCase && upperCase && numeric && specialConfirm) {
-//     generatePassword();
-
-//     }
-//   }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//   var password = []
+
+//   if(userInput > 8 || userInput < 128) {
+//     password.push(...userInput)
+//   }
+//   if(lowerCase) {
+    
+//     password.push(...lowerLetters)
+//   }
+//   if(upperCase) {
+//     var upperLetters = lowerLetters.toUpperCase()
+//     password.push(...upperLetters)
+//   }
+  
+//   password.textContent = password
+// }
+
