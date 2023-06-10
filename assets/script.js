8fa6f8d2d1faa0a8d98c39db1858dbcd0aa96396
Delante Randolph <delantetr@Delantes-iMac.local>
2023-06-10 10:18:12 -0400

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", '"', ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-var options = []
-var result = []
+// var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", '"', ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+// var options = []
+// var result = []
 
 function generatePassword() {
   console.log( "Click Confirmed");
 
-  // 1. Prompt the user for the password criteria
-  var userInput = parseInt(prompt("How many characters will your password be?", "Must be between 8 and 128 characters"));
-//    a. password length 8-128 characters
-  if(userInput < 8 || userInput > 128) {
-    alert("Please select a number between 8 and 128.");
-  //    b. include lowercase, uppercase, numeric, special
-  } else {
-      var lowerCase = confirm("Would you like to include lower case characters?")
-        if(lowerCase) {
-          options.push(lowerLetters)
-        }
-      var upperCase = confirm("Would you like to include upperCase characters?")
-        if(upperCase) {
-        options.push(...upperLetters)
-        }
-      // var numeric = confirm("Would you like to include numeric characters?")
-      //   if(numeric) {
-      //   options.push(numbers)
-      //   }
-      // var specialConfirm = confirm("Would you like to use special characters? (i.e. $,%,&)")
-      //   if(specialConfirm) {
-      //   options.push(specialChar)
-      //   }
-    }
+//   // 1. Prompt the user for the password criteria
+//   var userInput = parseInt(prompt("How many characters will your password be?", "Must be between 8 and 128 characters"));
+// //    a. password length 8-128 characters
+//   if(userInput < 8 || userInput > 128) {
+//     alert("Please select a number between 8 and 128.");
+//     //  b. include lowercase, uppercase, numeric, special
+//   } else {
+
+//   userInput = length
+//       var lowerCase = confirm("Would you like to include lower case characters?")
+//         if(lowerCase) {
+//           options.push(...lowerLetters)
+//         }
+//       var upperCase = confirm("Would you like to include upperCase characters?")
+//         if(upperCase) {
+//         options.push(...upperLetters)
+//         }
+//       var numeric = confirm("Would you like to include numeric characters?")
+//         if(numeric) {
+//         options.push(numbers)
+//         }
+//       var specialConfirm = confirm("Would you like to use special characters? (i.e. $,%,&)")
+//         if(specialConfirm) {
+//         options.push(specialChar)
+//         }
+//     }
         
 
-  // 2. Validate the input
-  // console.log(options)
+//   // 2. Validate the input
+//   console.log("Number of characters selected by user--------------");
+//   console.log(userInput);
+//   console.log("Options for password selection----------------------");
+//   console.log(options);
 
   // 3. Generate password
   
 
 
-  for(var i = 0; i < userInput; i++ ) {
-    // console.log(i)
-    var lowerLetter = Math.floor(Math.random() * lowerLetters.length);
-    console.log(lowerLetter[i])
+  // 
+  //   console.log(lowerLetter[i])
     // result.push(lowerLetters[lowerLetter])
   // }
   // for(var i = 0; i < userInput; i++) {
     // var upperLetter = Math.floor(Math.random() * upperLetters.length);
     // result.push(upperLetters[upperLetter])
-  }
+  // }
     // var numeral = Math.floor(Math.random() * numbers.length);
     // result.push(numbers[numeral])
     // var special = Math.floor(Math.random() * specialChar.length);
@@ -67,18 +70,19 @@ function generatePassword() {
   //   options = Math.floor(Math.random() * userInput.length)
   // }
 
-  return result;
+  // return result;
+
 }
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 
 
@@ -86,4 +90,4 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
